8ea930533242f389933591d838a0e4fff2978e34
Boiarski <diego_boiarski@hotmail.com>
2023-11-11 15:09:40 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toogleMode(){
+    const html = document.documentElement    
+    html.classList.toggle("light")
+    const img = document.querySelector('#profile img')
+    
+    if(html.classList.contains("light")){
+        img.setAttribute("src", "./assets/avatar-light.png");
+        img.setAttribute("alt", "Foto do Mayk light"); 
+    }
+    else{
+        img.setAttribute("src", "./assets/avatar.png");
+        img.setAttribute("alt", "Foto do Mayk dark");
+    }
+       
+}
+
